ed983268a809a96659f88df9cfed30b6fe8bc529
Molleti Bhargavi <68108779+bhargavim0607@users.noreply.github.com>
2021-05-20 16:15:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ import Subscribe from './components/subscribe';
 import Contact from './components/contact.jsx';
 import Footer from './components/footer.jsx';
 import './components/assets/css/style.css';
+
+import "./components/assets/css/bootstrap.min.css";
+import "./components/assets/css/default.css";
+import "./components/assets/css/magnific-popup.css";
+import "./components/assets/css/style.css";
+import "./components/assets/css/style.css.map";
+import "./components/assets/css/LineIcons.css";
 import './App.css'
 import './components/assets/js/vendor/jquery-1.12.4.min';
 import './components/assets/js/vendor/modernizr-3.6.0.min';
