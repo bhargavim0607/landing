5c7f25b1a879346e6a85d998d3b5310df28d673a
bhargavim0607 <bhargavim0607@gmail.com>
2021-05-20 14:47:09 +0530

    deploy

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ import Pricing from './components/pricing';
 import Subscribe from './components/subscribe';
 import Contact from './components/contact.jsx';
 import Footer from './components/footer.jsx';
-import './components/assets/css/default.css';
-import './components/assets/css/LineIcons.css';
 import './components/assets/css/style.css';
-import './components/assets/css/style.css.map';
 import './App.css'
 import './components/assets/js/vendor/jquery-1.12.4.min';
 import './components/assets/js/vendor/modernizr-3.6.0.min';
