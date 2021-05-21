2ffa54d640b6957f8376cea8a20bc19feb609a14
Molleti Bhargavi <68108779+bhargavim0607@users.noreply.github.com>
2021-05-21 10:49:04 +0530

    Add files via upload

---
+++ src/components/companies.jsx
@@ -0,0 +1,15 @@
+function companies(){
+    return(
+        <div class="Container" style={{paddingBottom:"120px",paddingTop:"120px",backgroundColor:"#f4f6f7",justifyContent:"space-around",width:"100%",overflowX:"hidden"}}>
+            <div class="row">
+                <div style={{margin:"auto"}}><img src="https://preview.uideck.com/items/start/assets/images/client_logo_01.png" alt=""/></div>
+                <div style={{margin:"auto"}}><img src="https://preview.uideck.com/items/start/assets/images/client_logo_02.png" alt=""/></div>
+                <div style={{margin:"auto"}}><img src="https://preview.uideck.com/items/start/assets/images/client_logo_03.png" alt=""/></div>
+                <div style={{margin:"auto"}}><img src="https://preview.uideck.com/items/start/assets/images/client_logo_04.png" alt=""/></div>
+            </div>
+
+        </div>
+    );
+}
+
+export default companies;
