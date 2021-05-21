21d8fc7e821095e8c7ffcbf453ffb4ffaa77c8ab
Molleti Bhargavi <68108779+bhargavim0607@users.noreply.github.com>
2021-05-21 10:48:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,18 +6,16 @@ import Pricing from './components/pricing';
 import Subscribe from './components/subscribe';
 import Contact from './components/contact.jsx';
 import Footer from './components/footer.jsx';
+import Companies from './components/companies.jsx';
 import './components/assets/css/style.css';
-
-import "./components/assets/css/bootstrap.min.css";
-import "./components/assets/css/default.css";
-import "./components/assets/css/magnific-popup.css";
-import "./components/assets/css/style.css";
-import "./components/assets/css/style.css.map";
-import "./components/assets/css/LineIcons.css";
-import './App.css'
-import './components/assets/js/vendor/jquery-1.12.4.min';
-import './components/assets/js/vendor/modernizr-3.6.0.min';
+import './components/assets/css/bootstrap.min.css'
+import './components/assets/css/default.css'
+import './components/assets/css/LineIcons.css'
+import './components/assets/css/magnific-popup.css'
+import './components/assets/css/style.css'
+import './components/assets/css/style.css.map'
 import './components/assets/js/main';
+import './App.css';
 
 
 
@@ -28,6 +26,7 @@ function App(){
        <Pricing/>
        <Subscribe/>
        <Testinomial/>
+       <Companies/>
        <Contact/>
        <Footer/>
     
